a08eaa128f25a360d4c67bda98604a53169f37d9
Ethan <sk18181@bristol.ac.uk>
2020-02-20 18:53:19 +0000

    written output function

--- readin.go
+++ readin.go
@@ -1,20 +1,34 @@
 package main
 
 import (
-	"main"
-    "bufio"
-    "fmt"
-    "io"
-    "io/ioutil"
-    "os"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
 )
 
-func readIn(){
-	d, err := os.Open("a_example.txt")
-	scanner :=  bufio.NewScanner(file)
-	for scanner.Scan(){
+func readIn() {
+	d, _ := os.Open("a_example.txt")
+	scanner := bufio.NewScanner(d)
+	for scanner.Scan() {
 		fmt.Println(scanner.Text())
 	}
 	//data =: Dataset{books = }
-	
+
+}
+
+func write(o Output) {
+	f, _ := os.Create("out.txt")
+	w := bufio.NewWriter(f)
+
+	w.WriteString(strconv.Itoa(o.numLibraries) + "\n")
+
+	for _, l := range o.libraryScans {
+		w.WriteString(strconv.Itoa(l.libraryID) + " " + strconv.Itoa(l.numBooks) + "\n")
+		for _,b:=l.books{
+			w.WriteString(strconf.Itoa(b))
+		}
+		w.WriteString("\n")
+	}
+	w.Flush()
 }
