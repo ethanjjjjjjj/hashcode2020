42d49289a221cf45434f3fdeb385d2b817ed6e16
Cassi <od18309@bristol.ac.uk>
2020-02-20 18:58:35 +0000

    Auto stash before merge of "master" and "origin/master"

--- readin.go
+++ readin.go
@@ -32,3 +32,33 @@ func write(o Output) {
 	}
 	w.Flush()
 }
+func readIn(){
+	file, err := os.Open("a_example.txt")
+ 
+	if err != nil {
+		log.Fatalf("failed opening file: %s", err)
+	}
+ 
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var txtlines []string
+ 
+	for scanner.Scan() {
+		txtlines = append(txtlines, scanner.Text())
+	}
+ 
+	file.Close()
+
+	books, _ := strconv.Atoi(string(txtlines[0][0]))
+	numlibs, _:= strconv.Atoi(string(txtlines[0][2]))
+	days, _:= strconv.Atoi(string(txtlines[0][4]))
+	
+	dataset := Dataset{books:books,numLibraries:numlibs,day:days}
+	fmt.Println(dataset)
+
+	fmt.Println(string(txtlines[0][0]))
+	for _, eachline := range txtlines {
+		fmt.Println(eachline)
+	}
+	
+}
