af2e634d63af86d93f56712512caad20696bfe71
Cassi <od18309@bristol.ac.uk>
2020-02-20 19:52:36 +0000

    made readin func

--- readin.go
+++ readin.go
@@ -32,6 +32,7 @@ func write(o Output) {
 	}
 	w.Flush()
 }
+
 func readIn(){
 	file, err := os.Open("a_example.txt")
  
@@ -49,11 +50,49 @@ func readIn(){
  
 	file.Close()
 
+
 	books, _ := strconv.Atoi(string(txtlines[0][0]))
 	numlibs, _:= strconv.Atoi(string(txtlines[0][2]))
 	days, _:= strconv.Atoi(string(txtlines[0][4]))
+
+	scores := string(txtlines[1])
+	bookList := make([]Book,books)
+	id := 0
+	s := ""
+	for _,c := range scores{
+		if unicode.IsSpace(c){
+			theScore,_ := strconv.Atoi(s)
+			newBook := Book{bookID:id,score:theScore}
+			bookList[id] = newBook
+			s = ""
+			id++
+		}else{
+			s = s +string(c)
+		}	
+	}
+
+	librarys := make([]Library,numlibs)
+	for i:=0; i < numlibs; i++{
+		numbooks,_:= strconv.Atoi(string(txtlines[2*i+2][0]))
+		signtime,_ := strconv.Atoi(string(txtlines[2*i+2][2]))
+		bookspday,_:= strconv.Atoi(string(txtlines[2*i+2][4]))
+		booklist:= make([]int,numbooks)
+		id := 0
+		s := ""
+		for _, c := range string(txtlines[2*i+3]){
+			if unicode.IsSpace(c){
+				theScore,_:=strconv.Atoi(s)
+				booklist[id] = theScore
+				s = ""
+				id++
+			}else{
+				s = s +string(c)
+			}
+		}
+		librarys[i] = Library{libraryID:i,numBooks:numbooks,signupTime:signtime,booksPerDay:bookspday,books:booklist}
+	}
  
-	dataset := Dataset{books:books,numLibraries:numlibs,day:days}
+	dataset := Dataset{books:books,numLibraries:numlibs,day:days,libraries:librarys}
 	fmt.Println(dataset)
 	
 }
